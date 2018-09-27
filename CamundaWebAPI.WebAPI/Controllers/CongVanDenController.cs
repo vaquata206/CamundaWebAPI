9a18b30b3037ad4071c01960b30632a818c2dcb7
trungnt <trungnt@greenglobal.vn>
2018-09-27 15:06:41 +0700

    Tao cong van api

---
+++ CamundaWebAPI.WebAPI/Controllers/CongVanDenController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CamundaClient;
+using CamundaWebAPI.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CamundaWebAPI.WebAPI.Controllers
+{
+    [Route("api/congvanden")]
+    public class CongVanDenController : Controller
+    {
+        private CamundaEngineClient _client;
+
+        public CongVanDenController()
+        {
+            _client = new CamundaEngineClient();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CongVanDen congVanDen)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var jCongVanDen = JsonConvert.SerializeObject(congVanDen);
+
+                    var taskResponse = await _client.BpmnWorkflowService.StartProcessInstanceAsync("XuLyCongVanProcess", null, null);
+
+                    return Ok(Json(new { Message = "", ProcessId = "" }));
+                }
+                else
+                {
+                    return BadRequest(this.ModelState);
+                }
+            }
+            catch
+            {
+                return StatusCode(500, Json(new { Message = "" }));
+            }
+        }
+    }
+}
