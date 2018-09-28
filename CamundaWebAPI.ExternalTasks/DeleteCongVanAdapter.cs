6cda01396c66c487483d871a40e09830f094dfe8
Tannvq <tannvq@greenglobal.vn>
2018-09-28 11:28:27 +0700

    externalTask: Xoa cong van

---
+++ CamundaWebAPI.ExternalTasks/DeleteCongVanAdapter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using CamundaClient.Dto;
+using CamundaClient.ViewModel;
+using CamundaClient.Worker;
+using CamundaWebAPI.Core.Common;
+using CamundaWebAPI.Entity;
+using CamundaWebAPI.Repository.Repository;
+using CamundaWebAPI.ViewModel.Request;
+using Newtonsoft.Json;
+
+namespace CamundaWebAPI.ExternalTasks
+{
+    [ExternalTaskTopic("xoaCongVan")]
+    [ExternalTaskVariableRequirements("congVanDen")]
+    class DeleteCongVanAdapter : ExternalTaskAdapter
+    {
+        #region variables
+        private const string CongVanDen = "congVanDen";
+        private const string XoaCongVan = "xoaCongVan";
+        #endregion
+
+        protected override ResponseInformation ExecuteTask(ExternalTask externalTask, ref Dictionary<string, object> resultVariables)
+        {
+            var response = new ResponseInformation() {
+                StatusResponse = ResponseInformation.Status.Successed
+            };
+
+            var isXoaCongVan = false;
+
+
+            try
+            {
+                var cvd = ExternalTaskHelper.GetVariable<CongVanDenRequest>(externalTask.Variables, CongVanDen);
+                if (cvd != null)
+                {
+                    using (var uow = new UnitOfWork(ConfigSettings.ConnectionString))
+                    {
+                        var entity = uow.CongVanDenRepository.Get<CongVanDen>(cvd.CongVanDenId);
+                        if (entity == null)
+                        {
+                            throw new Exception("The CongVanDen is null");
+                        }
+
+                        entity.DaXoa = true;
+                        uow.CongVanDenRepository.Update<CongVanDen>(entity);
+                        uow.Commit();
+                    }
+
+                    isXoaCongVan = true;
+                }
+                else
+                {
+                    response.StatusResponse = ResponseInformation.Status.Failed;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.StatusResponse = ResponseInformation.Status.Failed;
+                response.Message = ex.ToString();
+            }
+            response.Variables.Add(XoaCongVan, isXoaCongVan);
+            response.Variables = resultVariables;
+
+            return response;
+        }
+    }
+}
