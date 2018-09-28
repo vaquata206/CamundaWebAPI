e01f254fa6ea827b4f395385c451c659dd24e97a
trungnt <trungnt@greenglobal.vn>
2018-09-28 10:52:19 +0700

    Tao chi dao

---
+++ CamundaWebAPI.ViewModel/Request/ChiDaoRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CamundaWebAPI.ViewModel.Request
+{
+    public class ChiDaoRequest : BaseRequest
+    {
+        public Guid? ChiDaoId { get; set; }
+        public Guid CongVanDenId { get; set; }
+        public string NoiDung { get; set; }
+        public Guid NguoiChiDaoId { get; set; }
+        public string PhongBanThucHien { get; set; }
+    }
+}
---
+++ CamundaWebAPI.WebAPI/Controllers/ChiDaoController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CamundaClient;
+using CamundaWebAPI.Core.Common;
+using CamundaWebAPI.Entity;
+using CamundaWebAPI.Repository.IReposirory;
+using CamundaWebAPI.Repository.Repository;
+using CamundaWebAPI.ViewModel.Request;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CamundaWebAPI.WebAPI.Controllers
+{
+    [Route("api/chidao")]
+    public class ChiDaoController : Controller
+    {
+        private CamundaEngineClient _client;
+        private IUnitOfWork _uow;
+        
+        public ChiDaoController(CamundaEngineClient client, IUnitOfWork uow)
+        {
+            this._client = client;
+            this._uow = uow;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string congVanId)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(congVanId))
+                {
+                    return BadRequest(Json(new { Message = "The variables are not null or empty" }));
+                }
+
+                var congVan = this._uow.CongVanDenRepository.GetAsync<CongVanDen>(congVanId);
+
+                return Ok());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] string taskId, [FromBody] ChiDaoRequest chiDao)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (string.IsNullOrEmpty(chiDao.NoiDung) || string.IsNullOrEmpty(chiDao.PhongBanThucHien))
+                    {
+                        return BadRequest(Json(new { Message = "The variables are not null or empty" }));
+                    }
+
+                    var jChiDao = JsonConvert.SerializeObject(chiDao);
+
+                    await _client.BpmnWorkflowService.CompleteTask(taskId, new Dictionary<string, object> {
+                        { "variables", jChiDao}
+                    });
+
+                    return Ok(Json(new { Message = "Completed chi dao user task!" }));
+                }
+                else
+                {
+                    return BadRequest(this.ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.ToString());
+            }
+        }
+    }
+}
