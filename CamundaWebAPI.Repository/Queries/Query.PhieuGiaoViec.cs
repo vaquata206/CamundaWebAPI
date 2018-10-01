51812f45a7877e267addd36cf755d2a06877f93e
Tannvq <tannvq@greenglobal.vn>
2018-10-01 08:31:18 +0700

    update status

---
+++ CamundaWebAPI.Repository/Queries/Query.PhieuGiaoViec.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CamundaWebAPI.Repository.Queries
+{
+    public partial class Query
+    {
+        public const string GetPhieuGiaoViecByCongViecPhongBan = @"SELECT pgv. * 
+                FROM [PhieuGiaoViec] as pgv
+                JOIN [CongViecPhongBan_PhieuGiaoViec] as cvpb_pgv ON cvpb_pgv.PhieuGiaoViecId = pgv.PhieuGiaoViecId
+                WHERE cvpb_pgv.CongViecPhongBanId = @CongViecPhongBanId";
+    }
+}
--- CamundaWebAPI.Repository/Repository/PhieuGiaoViecRepository.cs
+++ CamundaWebAPI.Repository/Repository/PhieuGiaoViecRepository.cs
@@ -1,7 +1,13 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 using CamundaWebAPI.Entity;
+using CamundaWebAPI.Repository.Common;
 using CamundaWebAPI.Repository.IReposirory;
+using CamundaWebAPI.Repository.Queries;
 using CamundaWebAPI.Repository.Repository;
+using Dapper;
 
 namespace CamundaWebAPI.Repository.Reposirory
 {
@@ -9,6 +15,19 @@ namespace CamundaWebAPI.Repository.Reposirory
     {
         public PhieuGiaoViecRepository(IDbTransaction transaction) : base(transaction)
         {
+        }
+
+        public async Task<PhieuGiaoViec> GetByCongViecPhongBan(Guid congViecPhongBanId)
+        {
+            return await this.Connection.QueryFirstOrDefaultAsync<PhieuGiaoViec>(
+                Query.GetPhieuGiaoViecByCongViecPhongBan,
+                param: new
+                {
+                    CongViecPhongBanId = congViecPhongBanId
+                },
+                transaction: this.Transaction,
+                commandTimeout: Constants.CommandTimeout
+                );
         }
     }
 }
