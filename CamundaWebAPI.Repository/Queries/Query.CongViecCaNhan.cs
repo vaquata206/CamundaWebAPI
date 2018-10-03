537baa33524f100d3d29d7f6a4e2db54d047237d
gg_baotn <baotn@greenglobal.vn>
2018-10-03 11:31:02 +0700

    update query

--- CamundaWebAPI.Repository/Queries/Query.CongViecCaNhan.cs
+++ CamundaWebAPI.Repository/Queries/Query.CongViecCaNhan.cs
@@ -6,14 +6,15 @@ namespace CamundaWebAPI.Repository.Queries
 {
     public partial class Query
     {
-        public const string GetCVCNByPhieuGiaoViec = @"SELECT [PhieuGiaoViecId]
-                                                  ,[NguoiGiaoId]
-                                                  ,[NoiDung]
-                                                  ,[NhanVienThucHien]
-                                                  ,[TrangThai]
-                                                  ,[NgayTao]
-                                                  ,[NgaySua]
-                                                  ,[DaXoa]
-                                              FROM [PhieuGiaoViec] WHERE PhieuGiaoViecId =  @PhieuGiaoViecId";
+        public const string GetCVCNByPhieuGiaoViec = @"SELECT [CongViecCaNhanId]
+                                                          ,[CaNhanId]
+                                                          ,[PhieuGiaoViecId]
+                                                          ,[NoiDungThucHien]
+                                                          ,[TrangThai]
+                                                          ,[NgayHoanThanh]
+                                                          ,[NgayTao]
+                                                          ,[NgaySua]
+                                                          ,[DaXoa]
+                                                      FROM [CongViecCaNhan] WHERE PhieuGiaoViecId =  @PhieuGiaoViecId";
     }
 }
